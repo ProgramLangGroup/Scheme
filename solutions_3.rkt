c0cf7efabe4214706b7ce67593f97f9e8171a824
Brandon <btgrant6@gmail.com>
2018-11-13 13:26:57 -0600

    Finished problem 3

---
+++ solutions_3.rkt
@@ -0,0 +1,11 @@
+; authors: Brandon Grant, Dylan Hensley, Ethan Twilley
+; date: 11/20/2018
+; description: finds the nth Fibonacci number
+
+(define (fib n)
+  (if (<= n 2)
+      1
+      (+ (fib (- n 1)) (fib (- n 2)))))
+
+
+(fib 6)
