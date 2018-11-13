c665776884e715a457ce1136cb42536124cb1370
Brandon <btgrant6@gmail.com>
2018-11-13 13:15:29 -0600

    Finished problem 2

--- solution_1.rkt
+++ solution_1.rkt
@@ -1,3 +1,7 @@
+; authors: Brandon Grant, Dylan Hensley, Ethan Twilley
+; date: 11/20/2018
+; description: evaluates expressions
+
 (+ (+ (* 4 7) (* 5 9)) (* 2 3)); expression a
 (+ (+ 32 43) 54); expression b
 (- (* 123 13) (/ (+ 93 103) 45)); expression c
--- solution_2.rkt
+++ solution_2.rkt
@@ -1,3 +1,8 @@
+; authors: Brandon Grant, Dylan Hensley, Ethan Twilley
+; date: 11/20/2018
+; description: program calculates average and lists appended value
+
+; part a
 (define (sum vars); calculates sum
   (if
     (null? vars)
@@ -10,15 +15,22 @@
     (/ (sum vars) (length vars))
 )
 
-
 (avg '(83 83 85 91 97)); calls average function
 
-    
-(define (L (letters '(a b c)))
-    (display letters)
+
+; part b
+(define L '(a b c))
+
+(define (func_i)
+    (append L L L)
 )
 
-(L)
+(define (func_ii)
+    (append (list L L) L)
+)
+
+(func_i)
+(func_ii)
 
 
 
